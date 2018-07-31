e7a53725edb4feb8e64e3552bab13f65a5207406
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-31 10:58:10 +0200

    able to move car on mouse click

---
+++ Assets/Scripts/RiderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiderController : MonoBehaviour {
+
+    public Rigidbody2D rbRider;
+    public float speed = 20.0f;
+
+    private bool move = false;
+	
+	// Update is called once per frame
+    private void Update () {
+        if(Input.GetMouseButtonDown(0))
+            move = true;
+
+        if (Input.GetMouseButtonUp(0))
+            move = false;
+	}
+
+    private void FixedUpdate()
+    {
+        if(move){
+            rbRider.AddForce(transform.right * speed * Time.fixedDeltaTime * 100.0f, ForceMode2D.Force);
+        }
+    }
+}
