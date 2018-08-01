edfa51ccd5a4cd3c2d054cc721cdfe6346abc753
Jakub Brehuv <kubek.breha@gmail.com>
2018-08-01 09:56:59 +0200

    generating terrain

--- Assets/Scripts/PlayBoardGenerator.cs
+++ Assets/Scripts/PlayBoardGenerator.cs
@@ -8,7 +8,7 @@ public class PlayBoardGenerator : MonoBehaviour {
     public GameObject rider;
 
     private Vector3 riderPosition;
-    private int moveForwardDistance = 15;
+    private int moveForwardDistance = 20;
     private int moveLineIndex = 0;
 
 	// Use this for initialization
@@ -29,11 +29,13 @@ public class PlayBoardGenerator : MonoBehaviour {
 
     private void MoveTileForward(){
         Debug.Log("move tile forward");
-        linesArray[moveLineIndex].transform.position = new Vector3(linesArray[moveLineIndex].transform.position.x + 55,
+        Debug.Log(moveLineIndex);
+        linesArray[moveLineIndex].transform.position = new Vector3(linesArray[moveLineIndex].transform.position.x + 80,
                                                                    linesArray[moveLineIndex].transform.position.y,
                                                                    linesArray[moveLineIndex].transform.position.z);
-        moveLineIndex++;
         if (moveLineIndex == 3) moveLineIndex = 0;
+        moveLineIndex++;
+
     }
 
 }
